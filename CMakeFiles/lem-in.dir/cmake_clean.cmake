ef4ec330482cbb13308c216830684d613b6bc9b1
artem <artemkush1@gmail.com>
2020-11-25 16:36:55 +0000

    fix

---
+++ CMakeFiles/lem-in.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lem-in.dir/main.cpp.o"
+  "lem-in.pdb"
+  "lem-in"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lem-in.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
